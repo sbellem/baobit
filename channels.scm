ade2655749244555285cd965e12c6c1db63d230c
Sylvain Bellemare <125458+sbellem@users.noreply.github.com>
2026-02-06 19:10:40 +0900

    Add channels.scm

---
+++ channels.scm
@@ -0,0 +1,14 @@
+;; Channels configuration for baochan
+;; Usage: guix time-machine --channel=channels.scm -- build --load-path=packages -e '(@ (rust-xous) rust-xous)'
+
+(list
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  (branch "rust-team")
+  (commit "71f6e64afaa580a99aaea67ffd39bd4a40a8293d")
+  (introduction
+    (make-channel-introduction
+      "9edb3f66fd807b096b48283debdcddccfea34bad"
+      (openpgp-fingerprint
+        "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
