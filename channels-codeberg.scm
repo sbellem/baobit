c60a16b4712ae9dbb04dadd2a6c8c3ee239999e0
Sylvain Bellemare <125458+sbellem@users.noreply.github.com>
2026-02-06 19:33:01 +0900

    Add channel for codeberg

---
+++ channels-codeberg.scm
@@ -0,0 +1,14 @@
+;; Channels configuration for baochan (Codeberg mirror)
+;; Fallback when Savannah is unavailable
+
+(list
+ (channel
+  (name 'guix)
+  (url "https://codeberg.org/guix/guix.git")
+  (branch "rust-team")
+  (commit "71f6e64afaa580a99aaea67ffd39bd4a40a8293d")
+  (introduction
+    (make-channel-introduction
+      "9edb3f66fd807b096b48283debdcddccfea34bad"
+      (openpgp-fingerprint
+        "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
