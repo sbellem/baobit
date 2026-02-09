14f320ff92072bc48ee5d7aacc0acc777d7cd206
Sylvain Bellemare <125458+sbellem@users.noreply.github.com>
2026-02-10 05:33:06 +0900

    Update channels to latest rust-team branch

--- channels-codeberg.scm
+++ channels-codeberg.scm
@@ -4,9 +4,9 @@
 (list
  (channel
   (name 'guix)
-  (url "https://codeberg.org/guix/guix.git")
-  (branch "rust-team")
-  (commit "bb39e69d3865da3745b107fac95ae0e7087f529a")
+  (url "https://codeberg.org/gluonix/guix.git")
+  ;; tag rust-team-v1.5.0-3488-g4ad3747669
+  (commit "4ad3747669d7fbda177850cff1d4a9d6478adb84")
   (introduction
     (make-channel-introduction
       "9edb3f66fd807b096b48283debdcddccfea34bad"
