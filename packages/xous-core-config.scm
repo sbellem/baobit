2bf1dcc863f17f3be6a79214edcee87119443feb
Sylvain Bellemare <sbellem@gmail.com>
2026-02-13 04:35:12 +0900

    Use --git-describe and --git-rev CLI args
    Remove inline patching of sign_image.rs and swap_writer.rs.
    Pass version info via new CLI arguments instead.
    
    Also rename config variables for clarity:
    - %xous-version → %xous-git-describe
    - %xous-hash → %xous-guix-hash
    
    Add upstream tag fetching for forks in xous-core-info.scm.

--- packages/xous-core-config.scm
+++ packages/xous-core-config.scm
@@ -1,5 +1,5 @@
 (define-module (xous-core-config)
-  #:export (%xous-commit %xous-hash %xous-version %xous-clone-depth %xous-owner
+  #:export (%xous-commit %xous-guix-hash %xous-git-describe %xous-clone-depth %xous-owner
             %rust-version))
 
 ;;; Xous-core release configuration
@@ -15,14 +15,14 @@
 ;; GitHub owner (user or org)
 (define %xous-owner "betrusted-io")
 
-(define %xous-version "v0.10.0-25-g94e4fc28d")
+(define %xous-git-describe "v0.10.0-30-g7c98a8689")
 
-(define %xous-commit "94e4fc28d8976e49375ca583c5ff08b62e001c4a")
+(define %xous-commit "7c98a868999861b5b029eb779e2c81be8ceda9da")
 
-(define %xous-hash "15d0dyc312cplya0w71waqlvlslxmlcjlvvgm7k814d3am811hla")
+(define %xous-guix-hash "180glaiwd2mkc4cmb19i4qzkspb30srgl4shp6ngqfzrdsz72xdg")
 
 ;; Git clone depth for xous-core-info.scm (increase if git describe fails)
-(define %xous-clone-depth 50)
+(define %xous-clone-depth 30)
 
 ;; Rust toolchain version (e.g., "1.90", "1.91")
 ;; Package modules resolve this to rust-X.YZ
