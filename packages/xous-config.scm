df122d90a6498758f7ee2f8849aa308a95bc3b48
Sylvain Bellemare <sbellem@gmail.com>
2026-02-13 17:23:10 +0900

    Test xous-core pr 819

--- packages/xous-config.scm
+++ packages/xous-config.scm
@@ -13,10 +13,10 @@
 ;;; increase %xous-clone-depth (need more history to reach a tag).
 
 ;; GitHub owner (user or org)
-(define %xous-owner "betrusted-io")
-(define %xous-git-describe "v0.10.0-30-g7c98a8689")
-(define %xous-commit "7c98a868999861b5b029eb779e2c81be8ceda9da")
-(define %xous-guix-hash "180glaiwd2mkc4cmb19i4qzkspb30srgl4shp6ngqfzrdsz72xdg")
+(define %xous-owner "sbellem")
+(define %xous-git-describe "v0.10.0-31-gc98f84697")
+(define %xous-commit "c98f8469702eb08fbd2a5ad06b617c7e3286976f")
+(define %xous-guix-hash "13vvkaxzr5rfkgm62z8rh96njw01f2m51kxkqv8kgl1p6yci9vxm")
 
 ;; Git clone depth for xous-core-info.scm (increase if git describe fails)
 (define %xous-clone-depth 30)
