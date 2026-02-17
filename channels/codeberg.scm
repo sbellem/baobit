cdcddb385377d7ad9c4bdee4c43435728f3e4f15
Sylvain Bellemare <sbellem@gmail.com>
2026-02-17 17:56:35 +0900

    Update channels to soon-to-be-merged rust-team

--- channels/codeberg.scm
+++ channels/codeberg.scm
@@ -4,8 +4,8 @@
 (list (channel
         (name 'guix)
         (url "https://codeberg.org/gluonix/guix.git")
-        ;; tag rust-team-v1.5.0-3488-g4ad3747669
-        (commit "4ad3747669d7fbda177850cff1d4a9d6478adb84")
+        (branch "rust-team-v1.5.0-4274-ga832f7e68d")
+        (commit "a832f7e68d098837bc81e05b03d78adfa13e6cf7")
         (introduction
          (make-channel-introduction "9edb3f66fd807b096b48283debdcddccfea34bad"
           (openpgp-fingerprint
