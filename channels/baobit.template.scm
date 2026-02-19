e0ceaee444b1659a18d598a5270edd4bd2c8434e
Sylvain Bellemare <sbellem@gmail.com>
2026-02-19 16:13:00 +0900

    Update to latest guix rust-team commit

--- channels/baobit.template.scm
+++ channels/baobit.template.scm
@@ -6,8 +6,8 @@
 (list (channel
         (name 'guix)
         (url "https://github.com/sbellem/guix.git")
-        (branch "rust-team-v1.5.0-4275-g30fccbbc49")
-        (commit "30fccbbc4945f968acb7505ca628e30350b9f1d2")
+        (branch "rust-team-v1.5.0-4384-gc678e65a77")
+        (commit "c678e65a77ee0d983bfd48cd23cdada30e172be8")
         (introduction
          (make-channel-introduction
           "9edb3f66fd807b096b48283debdcddccfea34bad"
