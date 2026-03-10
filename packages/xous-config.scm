e626719d6474f8ea42e7eb93a46af6e3ce36b6b2
Sylvain Bellemare <sbellem@gmail.com>
2026-03-10 22:40:05 +0800

    Bump to rust 1.93 and latest xous-core

--- packages/xous-config.scm
+++ packages/xous-config.scm
@@ -19,20 +19,20 @@
 (define %xous-owner
   "betrusted-io")
 (define %xous-git-describe
-  "v0.10.0-28-g31738d94e")
+  "v0.10.0-60-ga846f680a")
 (define %xous-commit
-  "31738d94efec851f4bad313af0b46759e7fb907d")
+  "a846f680a882b69595e7b5b0288ec7d4f0eda98d")
 (define %xous-guix-hash
-  "022wr7hp9x2037mj6g46rr42dshhrsy61z6k2bnk8bwpfm259aj4")
+  "199d7qwyp5s8ppps4h873fprwj6q8f6y1z2ir0pwj40fbhm4lx1y")
 
 ;; Rust toolchain version (e.g., "1.90", "1.91")
 ;; Package modules resolve this to rust-X.YZ
 (define %rust-version
-  "1.90")
+  "1.93")
 
 ;; betrusted-io/rust fork for Xous sysroot
 ;; Must match %rust-version (e.g., 1.90.0-xous branch)
 (define %rust-xous-commit
-  "ca03bea71ce37fac6696f67020d27d4172f65771")
+  "2ae864f7d4d42c73ab05f5e01265ea31ae81a86e")
 (define %rust-xous-guix-hash
-  "01y6dl7f7ag4pgagav0qp4chir90qraidqsh7r8ml97mcsxwfkfl")
+  "0lh2ja680clqc5clcch8av8505rk5s71nkdg21yj4c7w5h24bmay")
